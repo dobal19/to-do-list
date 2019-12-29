51d101d07e347d46e75cd1e982500c3dbe727a2d
dobal19 <dobalsachin97@gmail.com>
2019-12-29 11:48:23 +0530

    added some features to the project

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,25 +6,45 @@ class TodoList extends React.Component {
     super(props);
     this.state = {
       item_input: '',
-      items: ['item1', 'item2']
+      items: []
     };
+    this.update = this.update.bind(this);
+    this.addItem = this.addItem.bind(this);
   };
+  update(event) {
+    this.setState({
+      item_input: event.target.value
+    });
+    console.log('event', event.target.value)
+  }
+
+  addItem(){
+    this.setState(prev => {
+      return {
+        item_input: '',
+        items: prev.items.concat(prev.item_input)
+      };
+    });
+    
+  }
   render() {
     return (
       <div className="GrandParent">
         <span className="Todo">Todo</span>
-        <button>+</button>
+          <button className="button" onClick={this.addItem}>+</button>
         <ul className=" listParent">
           {
             this.state.items.map((item, i) =>
-              <li className="listItems" key={'item' + i}> {item}  </li>
+              <li className="listItems" key={'item' + i}> 
+                 {item} 
+               </li>
             )
           }
         </ul>
-        <input type="text"
-        value={this.state.item_value}
-        onChange={this.update}>
-        </input>
+        <input className="inputBox" type="text"
+          value={this.state.item_input}
+          onChange={this.update}></input>
+
       </div>
     )
   }
